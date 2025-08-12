bd4947ab5632666de60ce0ceb5c4807a97960d64
John Wynter <johnwynter55893@outlook.com>
2025-08-12 14:42:10 +1000

    Add favicon and remove dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
-  content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // was "class"
   theme: { extend: {} },
   plugins: [],
 }
